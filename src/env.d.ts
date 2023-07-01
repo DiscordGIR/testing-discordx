d0ae5b2bcebbfba4a2f2df74cc57a995771017f9
SlimShadyIAm <aamir@farooq.xyz>
2023-07-02 01:20:57 +0200

    chore: type defs for postgres environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,5 +2,9 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     BOT_TOKEN: string;
     MAIN_GUILD_ID: string;
+    PG_HOST: string;
+    PG_USER: string;
+    PG_PASS: string;
+    PG_DB: string;
   }
 }
